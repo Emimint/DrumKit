16160a0dea76bc09d44186556310dc80b30b3dda
Emimint <emilie.echevin@gmail.com>
2022-11-19 10:27:50 -0500

    troubleshoot event detection

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-
-'use strict';
+"use strict";
 
 const keys = document.querySelectorAll(".key"); // list of all key div elements
 
 // 1: Changing html element to play specific sound:
 function letsDrum(e) {
+  // Event can be TouchEvent or KeyEvent!!!
+  console.log(e.keyCode);
+  console.log(e);
+  console.log(e.touches[0].target.childNodes);
+  console.log(e.touches[0].target);
+
   const divInfo = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  console.log(divInfo);
+
   const audioInfo = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   if (!divInfo) return; // stop if none of the desired keys
   divInfo.classList.add("playing");
@@ -22,9 +29,36 @@ function revertTransition(e) {
 keys.forEach((x) => x.addEventListener("transitionend", revertTransition));
 
 // 3: Choosing event to listen to according to viewport:
-if (window.matchMedia("(max-width: 767px)").matches) {
-  // The viewport is less than 768 pixels wide
-  window.addEventListener("touchmove", letsDrum);
+if (
+  /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+    navigator.userAgent
+  )
+) {
+  // if using mobile:
+  window.addEventListener("touchstart", letsDrum);
 } else {
   window.addEventListener("keydown", letsDrum);
 }
+
+// window.addEventListener("touchstart", getTouchPosition);
+
+
+// Returning TouchEvent position:
+function getTouchPosition(e) {
+  console.log(`the user touched the screen!`);
+  var evt = typeof e.originalEvent === "undefined" ? e : e.originalEvent;
+  var touch = evt.touches[0] || evt.changedTouches[0];
+  let x = touch.pageX;
+  let y = touch.pageY;
+  console.log(x, y);
+}
+
+
+// Returning all div.key element (e.g. drum keys) positions:
+keys.forEach((key, i) => {
+  var rect = key.getBoundingClientRect();
+  console.dir(key);
+  console.log(
+    `key [${i}]: ${rect.top}, ${rect.right}, ${rect.bottom}, ${rect.left}`
+  );
+});
